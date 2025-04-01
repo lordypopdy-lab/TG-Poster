3284f6f7c4b6e6a92ac89b7ce878f7610f8b6402
lordypopdy-lab <deelordpopdy2@gmail.com>
2025-04-01 10:46:53 -0700

    safe

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from telethon import TelegramClient, events
 
-# Your API credentials (Get from https://my.telegram.org/apps)
 api_id = 14458814
 api_hash = "b1e1a2ffd6000df2ea7b40517523bbbb"
 
-# Source and destination channels
 source_channel = "xmcryptonews"
 destination_channel = "@BitclubChatGroup"
 
-# Initialize the Telegram Client
 client = TelegramClient("poster_session", api_id, api_hash)
 
-# Set to store processed message IDs
 processed_messages = set()
 
-# Function to copy messages (avoiding duplicates)
 @client.on(events.NewMessage(chats=source_channel))
 async def forward_message(event):
-    message_id = event.message.id  # Unique ID of the message
-    if message_id not in processed_messages:  # Check if already processed
-        processed_messages.add(message_id)  # Mark as processed
-        await client.send_message(destination_channel, event.message)
+    try:
+        message_id = event.message.id
+        if message_id not in processed_messages:
+            processed_messages.add(message_id)
+            await client.send_message(destination_channel, event.message)
+    except Exception as e:
+        print(f"Error forwarding message: {e}")
 
-# Start the client
 client.start()
 print("Bot is running...")
 client.run_until_disconnected()
